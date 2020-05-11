b3a3b9b3000d1af1e82c6597c9c5b0086d1af849
Anthony Passler <passleranthony@yahoo.com>
2020-05-11 13:21:20 +0100

    styled the app using SCSS

---
+++ test/system/questions_test.rb
@@ -0,0 +1,29 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+
+  test "Asking How are you? yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "How are you?"
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+  test "saying I am going to work yields a Great! response from the coach" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+end
